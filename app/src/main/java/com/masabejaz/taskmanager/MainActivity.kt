8f1ac13796e8cfb5e2915a49f2dbb615d4350724
Masab Ejaz <138084661+onlymasab@users.noreply.github.com>
2024-01-25 17:59:30 +0500

    Initial commit

--- app/src/main/java/com/masabejaz/taskmanager/MainActivity.kt
+++ app/src/main/java/com/masabejaz/taskmanager/MainActivity.kt
@@ -3,13 +3,32 @@ package com.masabejaz.taskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+<<<<<<< HEAD
 import androidx.compose.foundation.layout.fillMaxSize
+=======
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+>>>>>>> fd7ace7 (Initial commit)
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+<<<<<<< HEAD
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+=======
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+>>>>>>> fd7ace7 (Initial commit)
 import com.masabejaz.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +41,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+<<<<<<< HEAD
                     Greeting("Android")
+=======
+                    TaskView(title = stringResource(R.string.task_title), subtitle = stringResource(R.string.task_subtitle))
+>>>>>>> fd7ace7 (Initial commit)
                 }
             }
         }
@@ -30,6 +53,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+<<<<<<< HEAD
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
@@ -42,5 +66,16 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     TaskManagerTheme {
         Greeting("Android")
+=======
+fun TaskView(title: String, subtitle: String, modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.ic_task_completed)
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Image(painter = image, contentDescription = null)
+        Text(text = title, fontWeight = FontWeight.Bold, modifier = Modifier.padding(top = 24.dp, bottom = 8.dp))
+        Text(text = subtitle, fontSize = 16.sp)
+>>>>>>> fd7ace7 (Initial commit)
     }
 }
